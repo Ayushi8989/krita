f513d1c097f368c3c4241da32a3f5649151e8674
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-03-05 05:35:31 +0530

    add navigation link to sign up page

--- lib/ngo/ngo_signIn.dart
+++ lib/ngo/ngo_signIn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:krita/ngo/formInput_Card.dart';
+import 'package:krita/ngo/ngo_signup.dart';
 
 const color = Color.fromARGB(230, 247, 149, 30);
 
@@ -15,7 +17,6 @@ class _Ngo_SignInPageState extends State<Ngo_SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
-
       appBar: AppBar(
         leading: IconButton(
           onPressed: () {
@@ -26,11 +27,9 @@ class _Ngo_SignInPageState extends State<Ngo_SignInPage> {
           iconSize: 30,
           //replace with our own icon data.
         ),
-
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -46,98 +45,108 @@ class _Ngo_SignInPageState extends State<Ngo_SignInPage> {
 
             //SignUp Container starts here
             SizedBox(
-              height:MediaQuery.of(context).size.height - 279.0,
-
+              height: MediaQuery.of(context).size.height - 279.0,
               child: Container(
-              margin: const EdgeInsets.only(top: 100),
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(243, 243, 243, 243),
-
-                borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(40.0),
-                    bottom: Radius.circular(0.0)
-                  ),
-
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(34, 0, 0, 0),
-                    offset: Offset(10, -10),
-                    blurRadius: 15,
-                    spreadRadius: 6.0,
-                  )
-                ]
-              ),
-
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text('Sign In',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w500,
-                        color: color,
-                      ),
-                    ),
-
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        InputCard(
-                      child: Container(
-                                        margin: const EdgeInsets.all(14),
-                                        child: const TextField(
-                                          decoration: InputDecoration(
-                                              labelText: 'User Name',
-                                              border: InputBorder.none,
-                                              labelStyle: TextStyle(
-                                                color: Color.fromARGB(
-                                                    127, 117, 117, 117),
-                                                fontSize: 14,
-                                              )),
-                                        ),
-                                      ),
-                    ),
-
-                    InputCard(
-                      child: Container(
-                                        margin: const EdgeInsets.all(14),
-                                        child: const TextField(
-                                          decoration: InputDecoration(
-                                              labelText: 'Password',
-                                              border: InputBorder.none,
-                                              labelStyle: TextStyle(
-                                                color: Color.fromARGB(
-                                                    127, 117, 117, 117),
-                                                fontSize: 14,
-                                              )),
-                                        ),
-                                      ),
+                margin: const EdgeInsets.only(top: 100),
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(243, 243, 243, 243),
+                    borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(40.0),
+                        bottom: Radius.circular(0.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(34, 0, 0, 0),
+                        offset: Offset(10, -10),
+                        blurRadius: 15,
+                        spreadRadius: 6.0,
                       )
-                      ],
-                    ),
-
-                    SizedBox(
-                      height: 59,
-                      width: 350,
-                      child: FloatingActionButton.extended(
-                        onPressed: () {}, 
-                        label: const Text('Sign In',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
+                    ]),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                          color: color,
                         ),
-                        backgroundColor: color,
                       ),
-                    ),
-                    
-                  ],
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InputCard(
+                            child: Container(
+                              margin: const EdgeInsets.all(14),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'User Name',
+                                    border: InputBorder.none,
+                                    labelStyle: TextStyle(
+                                      color: Color.fromARGB(127, 117, 117, 117),
+                                      fontSize: 14,
+                                    )),
+                              ),
+                            ),
+                          ),
+                          InputCard(
+                            child: Container(
+                              margin: const EdgeInsets.all(14),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                    labelText: 'Password',
+                                    border: InputBorder.none,
+                                    labelStyle: TextStyle(
+                                      color: Color.fromARGB(127, 117, 117, 117),
+                                      fontSize: 14,
+                                    )),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 59,
+                        width: 350,
+                        child: FloatingActionButton.extended(
+                          onPressed: () {},
+                          label: const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          backgroundColor: color,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account?',
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Ngo_SignUpPage(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Sign Up here',
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
             )
-            
           ],
         ),
       ),
