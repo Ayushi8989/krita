1d07046f0d1b2c8b884f7bfa191e827abd0173f6
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-03-07 23:57:07 +0530

    create user role page

--- lib/ngo/ngo_signUp.dart
+++ lib/ngo/ngo_signUp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:krita/ngo/formInput_Card.dart';
 import 'package:krita/ngo/ngo_signIn.dart';
 
---
+++ lib/ngo/user_role.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class userRole extends StatefulWidget {
+  const userRole({super.key});
+
+  @override
+  State<userRole> createState() => _userRoleState();
+}
+
+class _userRoleState extends State<userRole> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
