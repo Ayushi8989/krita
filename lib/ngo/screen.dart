08ce48833c55e66df949fc758cbd8955f1f0435a
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-04-02 21:49:16 +0530

    add bottom navigation bar

--- lib/ngo/screen.dart
+++ lib/ngo/screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:krita/constants.dart';
 
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class Screen extends StatefulWidget {
@@ -11,6 +12,13 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
+
+  var _bottomNavIndex = 0;
+  final iconList = <IconData>[
+    Bootstrap.house_door,
+    Bootstrap.chat_dots,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,6 +87,59 @@ class _ScreenState extends State<Screen> {
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Color.fromARGB(225, 255, 255, 255),
+          size: 55,
+        ),
+        backgroundColor: main_theme,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+            itemCount: iconList.length,
+            tabBuilder: (int index, bool isActive) {
+              // final color = isActive ? Colors.activeNavigationBarColor : colors
+              //     .notActiveNavigationBarColor;
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    iconList[index],
+                    size: 35,
+                    color: main_theme,
+                  ),
+                  const SizedBox(height: 4),
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 8),
+                  //   child: AutoSizeText(
+                  //     "brightness $index",
+                  //     maxLines: 1,
+                  //     style: TextStyle(color: color),
+                  //     group: autoSizeGroup,
+                  //   ),
+                  // )
+                ],
+              );
+            },
+            backgroundColor: Colors.white,
+            activeIndex: _bottomNavIndex,
+            splashColor: Colors.white,
+            // notchAndCornersAnimation: borderRadiusAnimation,
+            splashSpeedInMilliseconds: 300,
+            notchSmoothness: NotchSmoothness.defaultEdge,
+            gapLocation: GapLocation.center,
+            onTap: (index) => setState(() => _bottomNavIndex = index),
+            // hideAnimationController: _hideBottomBarAnimationController,
+            shadow: BoxShadow(
+              offset: Offset(0, 1),
+              blurRadius: 12,
+              spreadRadius: 0.5,
+              color: Color.fromARGB(225, 175, 175, 175),
+            ),
       ),
     );
   }
